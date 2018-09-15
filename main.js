1e47934dad9bdf827a0012828b849cf1f5f64d24
rishabhx64 <rishabharora1780@gmail.com>
2018-09-15 19:27:58 +0530

    xml requests

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-   $("#back-btn").css('display', "none");
-   $('#select_info').text('(For Multiple Selection of Events Press Ctrl+)');
-    $('#select_info_mobile').text('(For Multiple Selection of Events Press +)');
- })
+URL= "http://172.17.42.109:8000/registrations/";
 
-$(document).ready(function() {
-  $('#event-select').select2({
-    'placeholder': 'Search event name',
-    width: "resolve",
-  });
-   $('#college').select2({
-   
-    width: "resolve",
-  });
-});
+var xhr = new XMLHttpRequest();
+xhr.open('GET', URL , true);
+xhr.send();
+ 
+xhr.onreadystatechange = processRequest;
+ 
+function processRequest(e) {
+ 
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    var response = JSON.parse(xhr.responseText);
+        alert(response.colleges[0]);
+        alert(response.events[0]);
+}
+}
 
-console.clear();
+window.onload = function(f){
+var xhr = new XMLHttpRequest();
+  xhr.open("POST", URL, true);
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  
+  xhr.onreadystatechange = function() {
+      if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        // console.log(this.responseText);
+      }
+  }
+  myObj = { "events":"sadf", "email":"rishabbarora1780@gmail.com", "name":"rishabh", "gender":"male", "city":"noida", "phone":9911074100, "head_of_society":true, "year_of_study":2 };
+x=JSON.stringify({ events:["Best Event Ever","fghjk"], email:"rishabbarora1780@gmail.com", name:"rishabh", college:"BITS" , gender:"male", city:"noida", phone:9911074100, head_of_society:true, year_of_study:2 });
+// x=JSON.stringify(myObj);  
+xhr.send(x);
+}
