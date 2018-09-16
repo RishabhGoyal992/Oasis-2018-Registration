75267694b3061ca3d060bc11663dd87afbcb2890
rishabhx64 <rishabharora1780@gmail.com>
2018-09-16 10:01:53 +0530

    integration updated

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
-URL = "http://192.168.43.36:8000/2018/registrations/";
+$(document).ready(function () {
+  $("#back-btn").css('display', "none");
+  $('#select_info').text('(For Multiple Selection of Events Press Ctrl+)');
+  $('#select_info_mobile').text('(For Multiple Selection of Events Press +)');
+})
+
+function checkEmail() {
+
+  var email = document.getElementById('txtEmail');
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if (!filter.test(email.value)) {
+    'txtEmail'.textContent = 'Please provide a valid email address';
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
+  }
+}
+function phonenumber() {
+  var phoneno = /^\d{10}$/;
+  var mobn = document.getElementById('mob-number');
+  if (!phoneno.test(mobn.value)) {
+
+    alert("Provide a valid Mobile Number");
+    return false;
+
+  }
+  else {
+    return true;
+  }
+}
+
+function myFunction() {
+  checkEmail();
+  phonenumber();
+}
+
+
+$(document).ready(function() {
+  $('#event-select').select2({
+    'placeholder': 'Search event name',
+    width: "resolve",
+  });
+   $('#college').select2({
+
+    width: "resolve",
+  });
+});
+
+URL = "https://bits-oasis.org/2018/registrations/";
 $(document).ready(function () {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', URL, true);
@@ -16,13 +65,12 @@ $(document).ready(function () {
       var eventsOption = "";
 
       for (var i = 0; i < clgs.length; i++) {
-        // console.log(clgs[i]);
         collegeOption += "<option>" + clgs[i] + " </option>";
       }
 
       for (var i = 0; i < events.length; i++) {
-        // console.log(clgs[i]);
         eventsOption += "<option>" + events[i] + " </option>";
+        console.log(events[i]);
       }
 
       // console.log(collegeOption);
@@ -58,6 +106,7 @@ document.getElementById('submit-button').addEventListener('click', function () {
   var head_of_society;
   var year_of_study;
   var gender;
+  if(name!=""&&city!=""&&phone!=""&&email!=""&&college!=""){
   function gender() {
     if (document.getElementById('male').checked == true)
       gender = "male";
@@ -94,12 +143,15 @@ document.getElementById('submit-button').addEventListener('click', function () {
   xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      // console.log(this.responseText);
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200 && this.responseText.status==1) {
+      console.log(this.responseText);
+    }
+    else{
+      console.log(this.responseText);
     }
   }
   x = JSON.stringify({
-    events: ["Best Event Ever"],
+    events: ["test2"],
     email: email,
     name: name,
     college: college,
@@ -111,60 +163,12 @@ document.getElementById('submit-button').addEventListener('click', function () {
   });
   // console.log(x);
   xhr.send(x);
+}
 });
-
 
 //new
 
 
-// $(document).ready(function () {
-//   $("#back-btn").css('display', "none");
-//   $('#select_info').text('(For Multiple Selection of Events Press Ctrl+)');
-//   $('#select_info_mobile').text('(For Multiple Selection of Events Press +)');
-// })
-
-function checkEmail() {
-
-  var email = document.getElementById('txtEmail');
-  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-  if (!filter.test(email.value)) {
-    'txtEmail'.textContent = 'Please provide a valid email address';
-    alert('Please provide a valid email address');
-    email.focus;
-    return false;
-  }
-}
-function phonenumber() {
-  var phoneno = /^\d{10}$/;
-  var mobn = document.getElementById('mob-number');
-  if (!phoneno.test(mobn.value)) {
-
-    alert("Provide a valid Mobile Number");
-    return false;
-
-  }
-  else {
-    return true;
-  }
-}
-
-function myFunction() {
-  checkEmail();
-  phonenumber();
-}
-
-
-// $(document).ready(function() {
-//   $('#event-select').select2({
-//     'placeholder': 'Search event name',
-//     width: "resolve",
-//   });
-//    $('#college').select2({
-
-//     width: "resolve",
-//   });
-// });
 
 
 
